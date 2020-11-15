d2954a4775b3ee1a56e3b0b3e9d1ff7f48f946fb
kuziomavladimir <kuziomavladimir@yandex.ru>
2020-11-15 17:11:34 +0300

    add

--- src/main/java/homework2/Animals.java
+++ src/main/java/homework2/Animals.java
@@ -3,15 +3,14 @@ package homework2;
 public abstract class Animals {
     protected String name;
     protected int health;
-    protected int hunger;   // голод
-    protected int force;    // сила
+    protected int hunger;       // голод
+    protected int force;        // сила
+    protected boolean isAlife;  // жив или нет
 
-    public boolean isAlife; // жив или нет
-
-    public abstract void doVote();          // голос
-    public abstract void doMotion();        // движение
-    public abstract void checkHunger();     // проверка уровня голода
-    public abstract void doEat(Food food);           //приём пищи
+    public abstract void doVote();              // голос
+    public abstract void doMotion();            // движение
+    public abstract void checkHunger();         // проверка уровня голода
+    public abstract void doEat(Food food);      //приём пищи
 
     public void showAnimalCharacteristics() {
         // показать характеристики животного
